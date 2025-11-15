f00c697f6cfc20efc0d009d4b6aba3e2a1bb3aab
Bastian.Reschenhofer <bastian.reschenhofer@gmail.com>
2025-11-15 20:10:27 +0100

    added background

--- game.py
+++ game.py
@@ -5,6 +5,12 @@ pygame.init() #starts pygame
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('FirstWindow')
 clock = pygame.time.Clock()
+test_fond = pygame.font.Font('graphics\Fonts\PixelFont.ttf', 50)
+
+sky_surface = pygame.image.load('graphics\Sky.png')
+ground_surface = pygame.image.load('graphics\Ground.png')
+text_surface = test_fond.render('TestTxT', False, 'Black')
+
 
 #game loop
 while True:
@@ -13,10 +19,14 @@ while True:
             pygame.quit()
             exit()
 
+    
+    screen.blit(sky_surface, (0,0)) # blit -> block immage transfer
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300,50))
+    
+
     pygame.display.update()
     clock.tick(60) #not more than 60 loop cycles per sec
-
-    
 
 
 
