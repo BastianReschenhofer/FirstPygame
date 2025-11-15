1cb319b993f3ba69bb62e9ce8cd896578413c6dc
Bastian.Reschenhofer <bastian.reschenhofer@gmail.com>
2025-11-15 19:22:25 +0100

    fps managment

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ from sys import exit
 
 pygame.init() #starts pygame 
 screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('FirstWindow')
+clock = pygame.time.Clock()
 
 #game loop
 while True:
@@ -12,6 +14,7 @@ while True:
             exit()
 
     pygame.display.update()
+    clock.tick(60) #not more than 60 loop cycles per sec
 
     
 
