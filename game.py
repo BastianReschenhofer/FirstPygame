4c47dcae67a8b2e604ea25badd8256381433f875
Bastian.Reschenhofer <bastian.reschenhofer@gmail.com>
2025-11-15 20:33:26 +0100

    snowman

--- game.py
+++ game.py
@@ -11,6 +11,12 @@ sky_surface = pygame.image.load('graphics\Sky.png')
 ground_surface = pygame.image.load('graphics\Ground.png')
 text_surface = test_fond.render('TestTxT', False, 'Black')
 
+snowman_surface_orig = pygame.image.load('graphics\Snowman.png').convert_alpha()
+snowman_width, snowman_height = snowman_surface_orig.get_size()
+snowman_surface = pygame.transform.scale(snowman_surface_orig,(snowman_width * 4, snowman_height * 4))
+snowman_x_pos = 625
+snowman_y_pos = 200
+snowman_dir = True
 
 #game loop
 while True:
@@ -23,6 +29,13 @@ while True:
     screen.blit(sky_surface, (0,0)) # blit -> block immage transfer
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface,(300,50))
+    if(snowman_x_pos == 600): snowman_dir = False
+    if(snowman_x_pos == 650): snowman_dir = True
+    if(snowman_dir): snowman_x_pos -= 1 
+    else: snowman_x_pos +=1
+
+
+    screen.blit(snowman_surface,(snowman_x_pos,snowman_y_pos))
     
 
     pygame.display.update()
